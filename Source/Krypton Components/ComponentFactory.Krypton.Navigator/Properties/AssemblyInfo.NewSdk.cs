f26b789a3ec0d31e99a8ad59c568174e918b42ba
Thomas Bolon <tbolon@mb3m.fr>
2019-06-27 16:18:53 +0200

    revamp naming and versionning
    moved back to original dll name
    updated version to 6 to prevent conflicts
    created nuspec for prerelease

--- Source/Krypton Components/ComponentFactory.Krypton.Navigator/Properties/AssemblyInfo.NewSdk.cs
+++ Source/Krypton Components/ComponentFactory.Krypton.Navigator/Properties/AssemblyInfo.NewSdk.cs
@@ -1,11 +1,11 @@
 ﻿// *****************************************************************************
 // BSD 3-Clause License (https://github.com/ComponentFactory/Krypton/blob/master/LICENSE)
-//  © Component Factory Pty Ltd, 2006 - 2016, All rights reserved.
+//  © Component Factory Pty Ltd, 2006-2019, All rights reserved.
 // The software and associated documentation supplied hereunder are the 
 //  proprietary information of Component Factory Pty Ltd, 13 Swallows Close, 
 //  Mornington, Vic 3931, Australia and are supplied subject to license terms.
 // 
-//  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV) 2017 - 2020. All rights reserved. (https://github.com/Wagnerp/Krypton-NET-5.470)
+//  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV) 2017 - 2019. All rights reserved. (https://github.com/Wagnerp/Krypton-NET-5.470)
 //  Version 5.470.0.0  www.ComponentFactory.com
 // *****************************************************************************
 
@@ -15,24 +15,16 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.810.0")]
-[assembly: AssemblyInformationalVersion("6.0.810.0")]
-[assembly: AssemblyCopyright("© Component Factory Pty Ltd, 2006 - 2016. Then modifications by Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV) 2017 - 2020. All rights reserved.")]
-[assembly: AssemblyProduct("Krypton Navigator")]
+
 [assembly: AssemblyDefaultAlias("ComponentFactory.Krypton.Navigator.dll")]
-[assembly: AssemblyTitle("ComponentFactory.Krypton.Navigator")]
-[assembly: AssemblyCompany("Component Factory Pty Ltd, Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV)")]
-[assembly: AssemblyDescription("ComponentFactory.Krypton.Navigator")]
-[assembly: AssemblyConfiguration("Production")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: StringFreezing]
 [assembly: ComVisible(true)]
 [assembly: CLSCompliant(true)]
 [assembly: AllowPartiallyTrustedCallers()]
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
 [assembly: Dependency("System", LoadHint.Always)]
 [assembly: Dependency("System.Drawing", LoadHint.Always)]
 [assembly: Dependency("System.Windows.Forms", LoadHint.Always)]
 [assembly: Dependency("System.Xml", LoadHint.Always)]
 [assembly: Dependency("ComponentFactory.Krypton.Toolkit", LoadHint.Always)]
-[assembly: SecurityRules(SecurityRuleSet.Level1)]
